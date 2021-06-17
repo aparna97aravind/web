24de48a01a60281b451430cfe1877b32a9ac7a34
Aparna <aparna.pa@v2solutions.com>
2021-06-17 14:27:52 +0530

    Assignment completed

--- express1.js
+++ express1.js
@@ -4,43 +4,38 @@ const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser')
 
 app.use(fileUpload())
-
 app.set("view engine", "ejs")
-app.set(__dirname + '/import');
-
+app.set(__dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+let name = new Array;
+let password = new Array;
+
+app.get('/', (req, res) => {                         //GET METHOD
     res.sendFile(`${__dirname}/express.html`)
 })
 
-app.post('/aparna', (req, res) => {
+app.post('/aparna', (req, res) => {                  //POST METHOD
 
-    const password = req.body.password;
-    const username= req.body.username;
+    password.push(req.body.password);
+    name.push(req.body.username);
     let sampleFile;
     let uploadPath;
-    
-    //res.render("index", {username, password});
-
     if (!req.files || Object.keys(req.files).length === 0)
     {
-        return res.status(400).send('No files uploaded.');
-
+        return res.status(400).send('No files uploaded. Try again!');
     }
-    // The name of the input file sampleFile
     sampleFile= req.files.sampleFile;
     uploadPath = (`${__dirname}/save/${sampleFile.name}`);
-
-    sampleFile.mv(uploadPath, function (err) {
+    sampleFile.mv(uploadPath, function (err)
+    {
         if (err)
         {
             return res.status(500).send(err);
         }
     })
-    
-    res.render("index", {username, password});
-
+    //res.send('File uploaded!');
+    res.render("index", {name, password});
 })
 
 app.listen(8080, () => {
